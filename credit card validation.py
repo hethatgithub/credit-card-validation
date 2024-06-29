4b9a60da9dd593e6e2b7e813c37e2dee0dad1a31
hethatgithub <138364613+hethatgithub@users.noreply.github.com>
2024-06-29 14:12:01 +0530

    Add files via upload

---
+++ credit card validation.py
@@ -0,0 +1,31 @@
+card_no ="5610591081018250"
+odd_sum = 0
+even_sum = 0
+double_list = []
+number = list(card_no)
+print(number)
+for (idx,val) in enumerate(number):
+    if idx % 2 != 0: #this is an odd number
+        odd_sum += int(val)
+    else:            #this is an even number
+        double_list.append(int(val)*2)
+
+
+#converting the list into a string
+double_string = ""
+for x in double_list:
+    double_string += str(x)
+
+#converting the string back to a list
+double_list = list(double_string)
+
+for x in double_list:
+    even_sum += int(x)
+
+net_sum = odd_sum + even_sum
+if net_sum % 10 == 0:
+    print('Valid card!')
+else:
+    print('Invalid card')
+
+
